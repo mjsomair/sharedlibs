8835151a3026a6f9a040d81d038cd54f07e605c9
mjsomair <97043931+mjsomair@users.noreply.github.com>
2022-03-18 22:31:45 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,7 +1,7 @@
 def add(x,y) {
-  echo "sum of \$5(x) and \$5(y) is \$5(x+y)" 
+  echo "sum of {5}(x) and {5}(y) is {5}(x+y)" 
 }
 
 def mul(x,y) {
-  echo "multiplication of \$5(x) and \$5(y) is \$5(x*y)" 
+  echo "multiplication of {5}(x) and {5}(y) is {5}(x*y)" 
 }
